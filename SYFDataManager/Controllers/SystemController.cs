2a606d52d8bc81d631646e2d0d4cbefc71b0bd26
PawelklosPL <16582@student.pwsz.elblag.pl>
2020-02-22 18:27:30 +0100

    Add new end point "System"
    - System endpoint was added.
    - System version information is now available.

---
+++ SYFDataManager/Controllers/SystemController.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SYFDataManager.Controllers
+{
+    public class SystemController : Controller
+    {
+        // GET: System/Version
+        public ActionResult Version()
+        {
+            return Content(JsonConvert.SerializeObject("0.1.2020.02.22"));
+        }
+
+    }
+}
